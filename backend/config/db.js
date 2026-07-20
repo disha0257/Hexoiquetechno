b50477b29f96de003479653b14cfc5fb8448a332
Disha Vyas <vyasdishh25@gmail.com>
2026-07-20 14:22:22 +0530

    Fix Render backend startup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+
   try {
+
     if (!process.env.MONGO_URI) {
+
       throw new Error(
-        "MONGO_URI is not defined in .env file"
+        "MONGO_URI is missing"
       );
+
     }
 
     const connection =
@@ -24,8 +28,10 @@ const connectDB = async () => {
       error.message
     );
 
-    process.exit(1);
+    throw error;
+
   }
+
 };
 
 export default connectDB;
